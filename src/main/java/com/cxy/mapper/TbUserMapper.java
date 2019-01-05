9fce369a3d05e8bd3d5cadf685845afb6081a147
蔡晓宇 <@XTM-01712281213>
2019-01-05 17:33:43 +0800

    使用generator整合Mybatis

---
+++ src/main/java/com/cxy/mapper/TbUserMapper.java
@@ -0,0 +1,7 @@
+package com.cxy.mapper;
+
+import com.cxy.pojo.TbUser;
+import com.cxy.utils.MyMapper;
+
+public interface TbUserMapper extends MyMapper<TbUser> {
+}
---
+++ src/main/java/com/cxy/pojo/TbUser.java
@@ -0,0 +1,119 @@
+package com.cxy.pojo;
+
+import javax.persistence.*;
+
+@Table(name = "tb_user")
+public class TbUser {
+    @Id
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String sex;
+
+    private String question;
+
+    private String answer;
+
+    private String email;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex
+     */
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return question
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * @param question
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * @return answer
+     */
+    public String getAnswer() {
+        return answer;
+    }
+
+    /**
+     * @param answer
+     */
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    /**
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
